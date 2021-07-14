93f94f94b4597de61c3b3a0923e19be12d9f78d2
Kapilast77 <85858757+Kapilast77@users.noreply.github.com>
2021-07-14 19:55:49 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -4,17 +4,30 @@ const Bodies = Matter.Bodies;
 
 let engine;
 let world;
-
+var ball;
 var ground;
 var left;
 var right;
 var top_wall;
+var btn1;
+var btn2;
 
 function setup() {
   createCanvas(400,400);
   engine = Engine.create();
   world = engine.world;
   
+  //creating buttons to activate force functions
+  btn1=createImg('right.png');
+  btn1.position(220,30);
+  btn1.size(50,50);
+  btn1.mouseClicked(hforce);
+
+  btn2=createImg('push.png');
+  btn2.position(50,30);
+  btn2.size(50,50);
+  btn2.mouseClicked(vforce);
+
   ground =new Ground(200,390,400,20);
   right = new Ground(390,200,20,400);
   left = new Ground(10,200,20,400);
@@ -22,6 +35,12 @@ function setup() {
  
   rectMode(CENTER);
   ellipseMode(RADIUS);
+  
+  var options={
+    restitution:0.9,
+  }
+  ball=Bodies.circle(200,100,20);
+  World.add(world,ball)
 }
 
 function draw() 
@@ -32,5 +51,13 @@ function draw()
   left.show();
   right.show();
   Engine.update(engine);
+  ellipse(ball.position.x,ball.position.y,20)
 }
-
+function hforce()
+{
+  Matter.Body.applyForce(ball,{x:0,y:0},{x:0.05,y:0})
+}
+function vforce()
+{
+  Matter.Body.applyForce(ball,{x:0,y:0},{x:0,y:-0.05})
+}
